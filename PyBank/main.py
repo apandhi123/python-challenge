7abe697d163393223ddc822ed8a5556978cf49eb
Alisha <pandhi.alisha@gmail.com>
2018-05-11 19:43:00 -0500

    Complete

--- PyBank/main.py
+++ PyBank/main.py
@@ -55,10 +55,140 @@ with open(csvpath) as csvfile:
             ddate = row[0]
             
 
-            # Add to the revenue_changes list
-            #  revenue_change.append(int(row[1]))
+csvpath1 = os.path.join('budget_data_2.csv')
+
+with open(csvpath1) as csvfile:
+
+    # CSV reader specifies delimiter and variable that holds contents
+    reader = csv.reader(csvfile)
+
+    next(reader,None)
+
+    data = list(reader) 
+
+    # Variables to Track
+
+    Total_months1 = 0
+    Total_Revenue1 = 0
+    Revenue_avg1 = 0
+    revenue_change1 = 0
+    prev_revenue1 = 0
+    greatest_inc1 = 0
+    greatest_dec1 = 9999999999999999999999
+    idate1 = ""
+    ddate1 = ""
+
+    Total_months1 = len(data)
+
+    for row in data:
+        Current_row = int(row[1])
+        Total_Revenue1 += int(row[1])
+
+       # Set the Revenue average
+        Revenue_avg1 = Total_Revenue1/Total_months1   
         
+        # Keep track of changes
+        revenue_change1 = int(row[1]) - prev_revenue1
+        
+        # Reset the value of prev_revenue to the row I completed my analysis
+        prev_revenue1 = int(row[1])
+
+        # Determine the greatest increase
+        if (revenue_change1 > greatest_inc1):
+            greatest_inc1 = revenue_change1
+            idate1 = row[0]
+            # greatest_inc1[0] = row["Date"]
+
+        if (revenue_change1 < greatest_dec1):
+            greatest_dec1 = revenue_change1
+            ddate1 = row[0]
             
+# This will allow us to create file paths across operating systems
+import os
+
+# Module for reading CSV's
+import csv
+
+csvpath1 = os.path.join('budget_data_2.csv')
+
+with open(csvpath1) as csvfile:
+
+    # CSV reader specifies delimiter and variable that holds contents
+    reader = csv.reader(csvfile)
+
+    next(reader,None)
+
+    data = list(reader) 
+
+    # Variables to Track
+
+    Total_months1 = 0
+    Total_Revenue1 = 0
+    Revenue_avg1 = 0
+    revenue_change1 = 0
+    prev_revenue1 = 0
+    greatest_inc1 = 0
+    greatest_dec1 = 9999999999999999999999
+    idate1 = ""
+    ddate1 = ""
+
+    Total_months1 = len(data)
+
+    for row in data:
+        Current_row = int(row[1])
+        Total_Revenue1 += int(row[1])
+
+       # Set the Revenue average
+        Revenue_avg1 = Total_Revenue1/Total_months1   
+        
+        # Keep track of changes
+        revenue_change1 = int(row[1]) - prev_revenue1
+        
+        # Reset the value of prev_revenue to the row I completed my analysis
+        prev_revenue1 = int(row[1])
+
+        # Determine the greatest increase
+        if (revenue_change1 > greatest_inc1):
+            greatest_inc1 = revenue_change1
+            idate1 = row[0]
+            # greatest_inc1[0] = row["Date"]
+
+        if (revenue_change1 < greatest_dec1):
+            greatest_dec1 = revenue_change1
+            ddate1 = row[0]
+            
+
+            # Add to the revenue_changes list
+            #  revenue_change1.append(int(row[1]))
+        
+
+ # show output  
+print("Financial Analysis")
+print("Total Months", Total_months1)
+print("Total Revenue", "$", Total_Revenue1)
+print("Revenue_avg", Revenue_avg1)
+print("revenue_change", revenue_change1)
+print("Greatest Increase: " + idate1 + " $"+str(greatest_inc1) +"\n")
+print("Greatest Decrease: " + ddate1 + " $"+str(greatest_dec1) +"\n")
+
+file_to_output = "PyBankAnswer1.txt"
+with open(file_to_output, "w") as txt_file:
+        txt_file.write("Total Months: ")
+        txt_file.write(str(Total_months1))
+        txt_file.write("\n")
+        txt_file.write("Total Revenue: ")
+        txt_file.write(str(Total_Revenue1))
+        txt_file.write("\n")
+        txt_file.write("Average Change: ")
+        txt_file.write(str(revenue_change1))
+        txt_file.write("\n")
+        txt_file.write("Greatest Increase: ")
+        txt_file.write(idate1 + " $"+str(greatest_inc1))
+        txt_file.write("\n")
+        txt_file.write("Greatest Decrease: ")
+        txt_file.write(ddate1 + " $"+str(greatest_dec1))
+        
+      
 
  # show output  
 print("Financial Analysis")
--- PyParagraph/main.py
+++ PyParagraph/main.py
@@ -2,15 +2,14 @@
 import os
 
 # Set file path
-txtpath = "C:\\Users\\apandhi\\Desktop\\UTAUS201804DATA2-Class-Repository-DATA\\03-Python\\HOMEWORK\\Instructions\\PyParagraph\\raw_data\\paragraph_1.txt"
-
-# Set file path
+txtpath = os.path.join("paragraph_1.txt")
 
 # Set all variables to default
 num_words = 0
 lines = 0
 sentences = 0
 num_chars = 0
+blanklines = 0
 
 with open(txtpath) as paragraph:
    #Find out word count
@@ -35,6 +34,43 @@ average_chars = num_chars/num_words
         
     #Average sentence lenght
 average_sentence = num_words/sentences
+
+txtpath1 = os.path.join("paragraph_2.txt")
+num_words1 = 0
+lines1 = 0
+sentences1 = 0
+num_chars1 = 0
+blanklines1 = 0
+
+with open(txtpath1) as paragraph:
+   #Find out word count
+   #print (paragraph.read())
+   for line in paragraph:
+       words = line.split()
+       num_words1 += len(words)
+       num_chars1 += len(line)
+        
+       lines1 += 1
+       if line.startswith('\n'):   
+            blanklines1 += 1
+       else:
+    # assume that each sentence ends with . or ! or ?
+    # so simply count these characters
+            sentences1 += line.count('.') + line.count('!') + line.count('?')
+    # create a list of words
+    # use None to split at any whitespace regardless of length
+    # so for instance double space counts as one space
+     #Average word lenght
+average_chars1 = num_chars1/num_words1
+        
+    #Average sentence lenght
+average_sentence1 = num_words1/sentences1
+
+print("Number of words: " + str(num_words1))
+print("Number of sentences: " + str(sentences1))
+print("Average characters per word: " + str(average_chars1))
+print("Average sentence lenght: "+str(average_sentence1)+"\n") 
+
 
 print("Number of words: " + str(num_words))
 print("Number of sentences: " + str(sentences))
